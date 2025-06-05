4ef0653ca9f37204bc648a09c4e1064d161b9c64
GreedyWolf <jonel.pabulayan@wvsu.edu.ph>
2025-06-05 15:37:39 +0800

    added changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import App from './index';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = createRoot(rootElement);
